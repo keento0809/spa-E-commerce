9f31e4035935423d26e95d1973326ee25f7015f9
KEENTO <handball0321@gmail.com>
2023-05-07 01:33:53 +0900

    Added basic footer

--- src/components/layout/Layout/index.tsx
+++ src/components/layout/Layout/index.tsx
@@ -1,7 +1,14 @@
+import Footer from "@/components/common/Footer";
+
 interface Props {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: Props) {
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <Footer />
+    </>
+  );
 }
