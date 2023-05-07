d61d733e9c29f6ec2e4e4db6827d5e04b5eac45d
KEENTO <handball0321@gmail.com>
2023-05-08 05:07:49 +0900

    Fixed the link in Footer

--- src/components/common/Hero/index.tsx
+++ src/components/common/Hero/index.tsx
@@ -18,7 +18,7 @@ function Hero() {
               </p>
               <a
                 href="#featuredProducts"
-                className="inline-flex items-center justify-center py-3 text-base font-medium text-center text-gray-900 border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                className="inline-flex items-center justify-center my-3 text-base font-medium text-center text-gray-900 border-gray-300 rounded-lg  focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:focus:ring-gray-800"
               >
                 <Button text="Explore" />
               </a>
