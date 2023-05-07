f85110f24a5da126994d67dee56c24252efdecfe
KEENTO <handball0321@gmail.com>
2023-05-08 01:53:42 +0900

    Added the down bounce icon

--- src/components/common/Hero/index.tsx
+++ src/components/common/Hero/index.tsx
@@ -1,34 +1,44 @@
 import { memo } from "react";
+import ChevronDoubleDownIcon from "../Icon/ChevronDoubleDownIcon";
 
 function Hero() {
   return (
-    <div className="h-screen flex justify-center items-center w-full">
-      <section className="bg-white dark:bg-gray-900">
-        <div className="grid max-w-screen-2xl px-12 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-          <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              Future E-commerce service is here
-            </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              Check out SPA Shop, a next generation online shopping platform
-              exceeds your satisfaction. Everything for customers.
-            </p>
-            <a
-              href="#featuredProducts"
-              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-            >
-              Explore
+    <>
+      <div className="h-screen flex justify-center items-center w-full">
+        <section className="bg-white dark:bg-gray-900">
+          <div className="grid max-w-screen-2xl px-12 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div className="mr-auto place-self-center lg:col-span-7">
+              <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+                Future E-commerce service is here
+              </h1>
+              <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                Check out SPA Shop, a next generation online shopping platform
+                exceeds your satisfaction. Everything for customers.
+              </p>
+              <a
+                href="#featuredProducts"
+                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              >
+                Explore
+              </a>
+            </div>
+            <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+              <img
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
+                alt="mockup"
+              />
+            </div>
+          </div>
+        </section>
+        <section className="mt-8 absolute bottom-10 left-1/2">
+          <div className="animate-bounce p-4 bg-white rounded-full">
+            <a href="#featuredProducts">
+              <ChevronDoubleDownIcon />
             </a>
           </div>
-          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
-              alt="mockup"
-            />
-          </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </>
   );
 }
 
--- src/features/ProductDetail/ProductDetailPage/ProductImageSection.tsx
+++ src/features/ProductDetail/ProductDetailPage/ProductImageSection.tsx
@@ -3,7 +3,7 @@ import { ProductProps } from "@/types/product";
 
 export default function ProductImageSection({ product }: ProductProps) {
   return (
-    <div className="w-full sm:w-96 md:w-8/12 lg:w-6/12 flex justify-center items-center lg:flex-row flex-col lg:gap-8 sm:gap-6 gap-4">
+    <div className=" bg-white sm:w-96 md:w-8/12 lg:w-6/12 flex justify-center items-center lg:flex-row flex-col lg:gap-8 sm:gap-6 gap-4">
       <div className="bg-white px-6 py-12">
         <div className="flex justify-center items-center w-80 h-500">
           <Image
