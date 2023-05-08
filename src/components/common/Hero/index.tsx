051c658101144f9f018e3ca278fda8391ca17a87
KEENTO <handball0321@gmail.com>
2023-05-08 12:57:18 +0900

    Fixed design for responsible in Nav and Hero components

--- src/components/common/Hero/index.tsx
+++ src/components/common/Hero/index.tsx
@@ -7,12 +7,12 @@ function Hero() {
     <>
       <div className="h-screen flex justify-center items-center w-full">
         <section className="bg-white dark:bg-gray-900">
-          <div className="grid max-w-screen-2xl px-12 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="grid max-w-screen-2xl px-8 lg:px-12 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div className="mr-auto place-self-center lg:col-span-7">
-              <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+              <h1 className="max-w-2xl mb-4 text-3xl lg:text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
                 Future E-commerce service is here
               </h1>
-              <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+              <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 text-sm md:text-lg lg:text-xl dark:text-gray-400">
                 Check out SPA Shop, a next generation online shopping platform
                 exceeds your satisfaction. Everything for customers.
               </p>
