03eb2972874bcc50162671fed3e01f8cbda1b74d
KEENTO <handball0321@gmail.com>
2023-05-09 10:09:53 +0900

    test commit

--- src/features/ProductDetail/ProductDetailPage/index.tsx
+++ src/features/ProductDetail/ProductDetailPage/index.tsx
@@ -6,7 +6,6 @@ interface Props {
   productDetailData: Product;
 }
 
-// Contents of product-detail page
 export default function ProductDetailPage({ productDetailData }: Props) {
   return (
     <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4 min-h-screen">
