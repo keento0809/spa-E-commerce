1c9dc48bb016d0a02afc0edb055d2c0ab2851ad6
KEENTO <handball0321@gmail.com>
2023-05-28 04:37:18 +0900

    fixed the setting for lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "**/*.(js,jsx)": ["eslint --cache --fix"],
+  "**/*.(js,jsx)": [() => "eslint --cache --fix"],
   "**/*.(ts|tsx)": [() => "npx tsc --noEmit", "eslint --cache --fix"],
 };
--- src/contexts/GlobalLoadingContext/context.ts
+++ src/contexts/GlobalLoadingContext/context.ts
@@ -5,7 +5,6 @@ interface GlobalLoadingCtx {
   setIsGlobalLoading: Dispatch<SetStateAction<boolean>>;
 }
 
-// Context API checking the global loading status
 const GlobalLoadingContext = createContext<GlobalLoadingCtx | null>(null);
 
 function useGlobalLoadingContext() {
